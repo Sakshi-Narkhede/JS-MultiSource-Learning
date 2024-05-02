58d1c6de078e6c1b672672d07e29620606adefaf
Sakshi-Narkhede <79746553+Sakshi-Narkhede@users.noreply.github.com>
2024-05-02 14:14:30 +0000

    Different operations

---
+++ 01_basics/04_operations.js
@@ -0,0 +1,13 @@
+// let value = 4
+// let negValue = -value
+// console.log(negValue); // -4
+// console.log(typeof negValue); // number
+
+// let str1 = "hello "
+// let str2 = "sakshi"
+// console.log((str1 + str2)); // hello sakshi
+
+console.log("1" + 2); // 12
+console.log(1 + "2"); // 12
+console.log("1" + 2 + 2); // 122
+console.log(1 + 2 + "2"); // 32
